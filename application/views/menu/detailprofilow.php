b5140e9b3d18e07a1532622da71059aba1484d4e
DESKTOP-FAMSFKF\bahrularif <bahrularif007@gmail.com>
2018-10-19 23:09:43 +0700

    newfile

---
+++ application/views/menu/detailprofilow.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+	
+<?php $this->load->view('user/head') ?>
+
+	<body class="home header-v1">
+		<!-- <div id="fakeloader"></div> -->
+		<!-- <div class="color-switcher" id="choose_color"> -->
+			<!-- <a href="#." class="picker_close"><i class="fas fa-cog fa-spin"></i></a> -->
+		<!-- </div> -->
+		<div id="page" class="site">
+			<!-- Mobile main menu -->
+			<a href="#" id="mobile-trigger"><i class="fa fa-list" aria-hidden="true"></i></a>
+			
+<?php $this->load->view('user/header') ?>
+<?php $this->load->view('user/mainnav') ?>
+<?php $this->load->view('user/headnav') ?>	
+
+
+
+<?php $this->load->view('user/wiget') ?>
+<?php $this->load->view('user/sosmed') ?>
+
+<?php $this->load->view('user/footer') ?>	
