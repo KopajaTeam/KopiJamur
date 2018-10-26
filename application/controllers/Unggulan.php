33d79b0f046ef3c8795c9d6e49ac677931fa315a
DESKTOP-J5EQLQP\Panji <panjibudi467@gmail.com>
2018-10-26 19:11:04 +0700

    fix crash

--- application/controllers/Unggulan.php
+++ application/controllers/Unggulan.php
@@ -1,12 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-<<<<<<< HEAD
-class Unggulan extends CI_Controller {
-=======
-class About_us extends CI_Controller {
->>>>>>> 360c0a78566de18b1b367b5dcd69f7183b2eb2c0
 
+class Unggulan extends CI_Controller {
 
 	public function index(){
 		$this->load->view('user/keyfet/unggulan');
