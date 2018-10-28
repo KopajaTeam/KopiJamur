e331c2a28940f4e4c10ae5036f0ed88879751409
DESKTOP-FAMSFKF\bahrularif <bahrularif007@gmail.com>
2018-10-28 23:06:46 +0700

    masih proses

--- application/views/menu/detailforum.php
+++ application/views/menu/detailforum.php
@@ -20,7 +20,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- <div id="content" class="site-content global-layout-right-sidebar"> -->
 				<div class="container">
 					<div class="section-title-wrap" style="text-align: center;">
-						<h2 class="section-title">Detail Forum</h2><span class="divider"></span>
+						<h2 class="section-title">
+
+							<?php echo 'Detail Forum' ?>
+							
+						</h2><span class="divider"></span>
 						<p class="section-subtitle"></p></div>	
 					<div class="inner-wrapper">
 						<div id="primary" class="content-area">
@@ -31,7 +35,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									</div>
 									<div class="entry-content-wrapper">
 										<header class="entry-header">
-											<h2 class="entry-title"><a href="#" rel="bookmark">Website Design & Development</a></h2>
+											<h2 class="entry-title"><a href="#" rel="bookmark">
+
+												<?php echo "Judul Forum"; ?>
+
+											</a></h2>
 										</header><!-- .entry-header -->
 										<div class="entry-meta">
 											<span class="posted-on"><a href="#" rel="bookmark"><span class="entry-date published updated">February 22, 2017</span></a>
@@ -41,7 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 										<span class="cat-links"><a href="#"  rel="category tag">Design</a></span>
 									</div><!-- .entry-meta -->
 									<div class="entry-content">
-										<p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit.Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quisProin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit.Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis</p>
+
+										<?php echo "ahayconten"; ?>		
+
 									</div><!-- .entry-content -->
 								</div><!-- .entry-content-wrapper -->
 
@@ -54,9 +64,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								</div>
 								<div class="author-info">
 									<h4 class="author-header">
-										Written by&nbsp;<a href="#" title="Posts by Director & Writer: Kopaja Team" rel="author">Director & Writer: Kopaja Team</a>   </h4>
+										Written by&nbsp;<a href="#" title="Posts by Director & Writer: Kopaja Team" rel="author"> 
+
+											<?php echo "Nama Penulis"; ?>
+												
+											</a></h4>
 										<div class="author-content">
-											<p>Phasellus imperdiet libero sitdl amet dlante Donec convallis elemen tumd leo. Etiam non pede aecen vehicu magna quis pede. Maecenas felis quam consectetuer dictum..</p>
+
+											<p><?php echo "Pesan Penulis"; ?></p>
+
 										</div>
 									</div> <!-- .author-info -->
 								</div> <!-- .authorbox -->
@@ -79,7 +95,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 												<footer class="comment-meta">
 													<div class="comment-author vcard">
 														<img alt="Author" src="<?php echo base_url('assets/images/blog-single')?>/author-2.jpg" class="avatar img-circle">
-														<b class="fn"><a href="#"  class="url">Anil</a></b>
+														<b class="fn"><a href="#"  class="url">
+
+															<?php echo "Nama User"; ?>
+															
+														</a></b>
 														<span class="says">says:</span>
 														<div class="reply">
 															<a  class="comment-reply-link" href="#">Reply</a>
@@ -87,12 +107,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 													</div><!-- .comment-author -->
 													<div class="comment-metadata">
 														<a href="#" >
-															<span> May 28, 2017 at 8:17 am </span>
+															<span><?php echo "Tanggal Komentar"; ?></span>
 														</a>
 													</div><!-- .comment-metadata -->
 												</footer><!-- .comment-meta -->
 												<div class="comment-content">
-													<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s,</p>
+													<p><?php echo "isi Komentar"; ?></p>
 												</div><!-- .comment-content -->
 
 											</article><!-- .comment-body -->
@@ -103,7 +123,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 															<div class="comment-author vcard">
 																<img alt="" src="<?php echo base_url('assets/images/blog-single')?>/author-3.jpg" class="avatar img-circle">
 																<b class="fn">
-																	<a href="#" class="url">John Mark</a> </b>
+																	<a href="#" class="url"><?php echo "Nama User"; ?></a> </b>
 																	<span class="says">says:</span>
 																	<div class="reply"><a rel="nofollow" class="comment-reply-link" href="#">Reply</a>
 																	</div>
@@ -111,12 +131,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 																<div class="comment-metadata">
 																	<a href="#" >
 																		<span>
-																			May 28, 2017 at 8:17 am  </span>
+																			<?php echo "Tanggal Komentar"; ?> </span>
 																		</a>
 																	</div><!-- .comment-metadata -->
 																</footer><!-- .comment-meta -->
 																<div class="comment-content">
-																	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s,</p>
+																	<p><?php echo "Isi Komentar"; ?></p>
 																</div><!-- .comment-content -->
 															</article><!-- .comment-body -->
 														</li><!-- #comment-## -->
@@ -129,24 +149,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 												</h3>
 												<form action="http://anilbasnet.com/demo/biz-lina/."  id="commentform" class="comment-form">
 													<p class="comment-notes">
-														<span id="email-notes">Your email address will not be published.
-														</span> Required fields are marked <span class="required">*</span>
+														<!-- <span id="email-notes">Your email address will not be published. -->
+														</span> Silakan Login Terlebih dahulu <span class="required">*</span>
 													</p>
 													<p class="comment-form-comment">
 														<label for="comment">Comment</label>
 														<textarea id="comment" name="comment"></textarea>
 													</p>
-													<p class="comment-form-author">
+													<!-- <p class="comment-form-author">
 														<label for="author">Name <span class="required">*</span></label>
 														<input id="author" name="author" type="text">
-													</p>
-													<p class="comment-form-email">
+													</p> -->
+													<!-- <p class="comment-form-email">
 														<label for="email">Email <span class="required">*</span></label>
 														<input id="email" name="email" type="email" aria-describedby="email-notes">
-													</p>
-													<p class="comment-form-url"><label for="url">Website</label>
+													</p> -->
+													<!-- <p class="comment-form-url"><label for="url">Website</label>
 														<input id="url" name="url" type="url">
-													</p>
+													</p> -->
 													<p class="form-submit">
 														<input name="submit" type="submit" id="submit" class="submit" value="Post Comment">
 														<input type="hidden" id="comment-post-ID">
