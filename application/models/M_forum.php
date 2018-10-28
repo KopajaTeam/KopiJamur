62889bde6013d619e8bfab2a27c4cacddd1c8bfd
DESKTOP-FAMSFKF\bahrularif <bahrularif007@gmail.com>
2018-10-28 22:18:40 +0700

    sek belom tau bener apa ngk

--- application/models/M_forum.php
+++ application/models/M_forum.php
@@ -10,4 +10,25 @@ class M_forum extends CI_Model
     {
         parent::__construct();
     }
+
+       // insert data
+    function insert($data)
+    {
+        $this->db->insert($this->table, $data);
+    }
+
+    // update data
+    function update($kd, $data)
+    {
+		$this->db->update($this->table, $data);
+        //$this->db->update($this->table, $data);
+    }
+
+    // delete data
+    function delete($kd)
+    {
+         $this->db->delete($this->table, $data);
+       // $this->db->delete($this->table);
+    }
+
 }
